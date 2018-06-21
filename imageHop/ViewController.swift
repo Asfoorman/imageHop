665c67e2a98aef65c6b5d708022e99e208bbbca3
Anes Mehai <nsamehai@gmail.com>
2018-06-21 13:22:44 -0400

    got bunny to hop using hardcode. Next step is to make the inference and String Interpolation and Concatenation so bunnies hop without me getting the full name rather just the condition

--- imageHop/ViewController.swift
+++ imageHop/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var jumpBttn: UIButton!
+    
+    @IBOutlet weak var bunny1: UIImageView!
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+     
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var hopAnimation: [UIImage] = [
+            UIImage(named: "frame-1.png")!,
+            UIImage(named: "frame-2.png")!,
+            UIImage(named: "frame-3.png")!,
+            UIImage(named: "frame-4.png")!,
+            UIImage(named: "frame-5.png")!,
+            
+        ]
+        
+        bunny1.animationImages = hopAnimation
+        bunny1.animationDuration = 1.0
+        bunny1.animationRepeatCount = 4
+        bunny1.startAnimating()
+        bunny1.image = UIImage(named: "frame-1.png")
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
